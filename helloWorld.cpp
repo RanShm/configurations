74c4f96bc6f9fe69fc47a4987d4a0d783bfeb0c9
rans <>
2019-03-29 14:37:23 +0300

    CPP version for HelloWorld.

---
+++ helloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "please write something" << std::endl;
+    std::string inp;
+    
+    std::cin >> inp ;
+
+    std::cout << "you typed = " << inp << std::endl;
+
+return 0;
+}
